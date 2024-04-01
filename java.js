7f14f800e54f86fba627751ab9135485b28d9b56
Davi Prado <143515210+DaviBritoPrado@users.noreply.github.com>
2024-04-01 20:38:41 -0300

    Update java.js

--- java.js
+++ java.js
@@ -5,7 +5,12 @@ function media(){
     if(isNaN(nota1) || isNaN(nota2)  || isNaN(nota3)){
         let texto = document.querySelector("#ola");
         texto.innerHTML = "Digite um numero";
-    }else{
+    }else if(nota1 < 0 || nota2 < 0 || nota2 < 0){
+        let texto = document.querySelector("#ola");
+        texto.innerHTML = "todos os inputs devem possuir um numero inteiro";
+    }
+    
+    else{
         let resultado = (nota1+nota2+nota3)/3;
         let texto = document.querySelector("#ola");
         texto.innerHTML = resultado.toFixed(2)
